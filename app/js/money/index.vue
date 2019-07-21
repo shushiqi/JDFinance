df0fe61b0fd61c524653f3765636b374ebe64c91
shushiqi <1539588925@qq.com>
2019-07-22 02:44:42 +0800

    添加赚钱分页

--- app/js/money/index.vue
+++ app/js/money/index.vue
@@ -1,27 +1,49 @@
 <template lang="html">
     <div>
       <iHeader></iHeader>
-      <!-- <Slider></Slider> -->
-      <Footer></Footer>
+      <Slider></Slider>
+      <AdFirst></AdFirst>
+      <Money></Money>
+      <AdSecond></AdSecond> 
+      <Finance></Finance>  
+      <AdThird></AdThird>      
+      <Footer cname="moneyFooter"></Footer>
       <Navbar></Navbar>
     </div>
 </template>
 
 <script>
 import iHeader from "../public/header";
-// import Slider from './moneySlider'
-import Footer from '../public/footer'
-import Navbar from '../public/navbar'
+import Slider from "./moneySlider";
+import Footer from "../public/footer";
+import Navbar from "../public/navbar";
+import AdFirst from "./adFirst";
+import Money from "./money";
+import AdSecond from "./adSecond";
+import Finance from "./finance";
+import AdThird from "./adThird";
 export default {
   components: {
     iHeader,
-    // Slider,
+    Slider,
     Footer,
-    Navbar
+    Navbar,
+    AdFirst,
+    Money,
+    AdSecond,
+    Finance,
+    AdThird
   }
 };
 </script>
 
 <style lang="scss" >
-
+.moneyFooter {
+  margin-top: 0;
+  ul {
+    li:first-child {
+      display: none;
+    }
+  }
+}
 </style>
---
+++ app/js/money/moneySlider.vue
@@ -0,0 +1,45 @@
+<template>
+  <Panel title="轮播图" :class="$style.panel">
+    <section :class="$style.content">
+      <Slider :items="items" :cname="$style.slider" />
+    </section>
+  </Panel>
+</template>
+
+<script>
+import Panel from "../core/panel.vue";
+import Slider from "../core/slider.vue";
+export default {
+  components: {
+    Panel,
+    Slider
+  },
+  data(){
+    return{
+      items:[
+      {
+        href: "home",
+        src: '//img12.360buyimg.com/jrpmobile/jfs/t1/31588/6/14705/52443/5cbeb2b2Ea0648bba/e05f04db25d47126.png'
+      },
+      {
+        href: "home",
+        src: '//img12.360buyimg.com/jrpmobile/jfs/t1/38925/1/7734/32802/5cef91cfE8962dc90/08a7df363c6465f2.png'
+      }]
+    }
+  }
+};
+</script>
+<style lang="scss" module>
+@import "../../css/element.scss";
+.panel {
+  @include panel;
+  >h4{
+    display: none;
+  }
+  .content {
+    .slider {
+      margin-top: 120px;
+    }
+  }
+}
+</style>
