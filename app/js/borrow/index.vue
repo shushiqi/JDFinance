b5d626b426823f085ab2945e030ddc63191c4bc2
shushiqi <1539588925@qq.com>
2019-07-22 02:45:29 +0800

    添加借钱分页

---
+++ app/js/borrow/index.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <div>
+      <iHeader></iHeader>
+      <Slider></Slider>
+      <Welfare></Welfare>
+      <Service></Service>
+      <Footer></Footer>
+      <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import iHeader from "../public/header";
+import Slider from "./borrowSlider";
+import Footer from "../public/footer";
+import Navbar from "../public/navbar";
+import Welfare from './welfare'
+import Service from './service'
+export default {
+  components: {
+    iHeader,
+    Slider,
+    Footer,
+    Navbar,
+    Welfare,
+    Service
+  }
+};
+</script>
+
+<style lang="scss" >
+
+</style>
